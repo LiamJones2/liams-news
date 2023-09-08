d6bfbfa7112408f2aedb5e9f83ac3af5bcc258a2
liamjones2 <liammjoness@gmail.com>
2023-09-08 15:19:27 +0100

    votes missing colon on change

--- liams-news/src/Components/Votes/Votes.jsx
+++ liams-news/src/Components/Votes/Votes.jsx
@@ -3,25 +3,25 @@ import axios from 'axios'
 export function upvoteArticle(article_id) {
     const articleVotesDocument = document.getElementById(`article${article_id}votes`)
     let articleVotes = articleVotesDocument.textContent.split(" ").pop()
-    articleVotesDocument.textContent = "Votes " + (++articleVotes)
+    articleVotesDocument.textContent = "Votes: " + (++articleVotes)
 
     axios.patch(`https://nc-news-liam.onrender.com/api/articles/${article_id}`, {
         inc_votes: 1
     })
     .catch(() => {
-        articleVotesDocument.textContent = "Votes " + (--articleVotes)
+        articleVotesDocument.textContent = "Votes: " + (--articleVotes)
     })
 }
 
 export function downvoteArticle(article_id) {
     const articleVotesDocument = document.getElementById(`article${article_id}votes`)
     let articleVotes = articleVotesDocument.textContent.split(" ").pop()
-    articleVotesDocument.textContent = "Votes " + (--articleVotes)
+    articleVotesDocument.textContent = "Votes: " + (--articleVotes)
 
     axios.patch(`https://nc-news-liam.onrender.com/api/articles/${article_id}`, {
         inc_votes: -1
     })
     .catch(() => {
-        articleVotesDocument.textContent = "Votes " + (++articleVotes)
+        articleVotesDocument.textContent = "Votes: " + (++articleVotes)
     })
 }
